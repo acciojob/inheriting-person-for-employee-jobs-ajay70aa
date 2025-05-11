fa0110597990b0485381938c05315d094c07214f
D. Ajay Kumar <154305661+ajay70aa@users.noreply.github.com>
2025-05-11 11:27:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 // complete this js code
-function Person(name, age) {}
+function Person {
+	constructor(name, age){
+		this.name = name;
+		this.age= age;
+	}
+	greet(){
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+	}
+}
 
-function Employee(name, age, jobTitle) {}
-
+class Employee extends Person{
+	constructor(name, age, jobTitle){
+		super(name, age);
+		this.jobTitle = jobTitle
+	}
+	jobGreet(){
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
+	}
+}
+const employee1 = Employee("Ajay", 28, "Developer")
+console.log(employee1.greet())
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
